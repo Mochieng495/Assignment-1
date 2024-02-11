c5b010020b2f7bf0ad1fda734e1967ac726a40de
Mochieng495 <158850174+Mochieng495@users.noreply.github.com>
2024-02-11 22:20:11 +0300

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+      int a, b;
+    printf("Enter value of a and b\n");
+    scanf("%d %d", &a, &b);
+    printf("Addition of %d and %d is %d\n", a, b, (a + b));
+
+    return 0;
+}  
+
